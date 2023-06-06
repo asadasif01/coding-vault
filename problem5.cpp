9094cbdf2b743a9a0a05115892b1552647518ed4
Muhammad Asad <111152906+aliasad344@users.noreply.github.com>
2023-06-06 21:44:00 +0500

    LeetCode Problem 26 Remove Duplicates from Sorted Array

--- problem5.cpp
+++ problem5.cpp
@@ -1,8 +1,8 @@
 /******************************************************************************
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
 *******************************************************************************/
 
 #include <iostream>
