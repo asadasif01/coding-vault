c0a54f1c95fc281f6293f06bd071c96060df312b
Muhammad Asad <masadasif451@gmail.com>
2023-09-28 14:57:25 +0500

    fabnocci series

---
+++ Fabnocci Series using DP (memoization).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int f(vector<int> &dp, int n){
+    if(n <= 1){
+        return n;
+    }
+    
+    if(dp[n] != -1) return dp[n];
+    
+    return dp[n] = f(dp, n-1) + f(dp, n-2);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    
+    vector<int> dp(n+1, -1);
+    
+    int number = f(dp, n);
+    cout << number << endl;
+    return 0;
+}
