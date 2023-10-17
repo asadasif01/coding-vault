6126e62e5bfba8df1def0b61399153d4f71898d8
Muhammad Asad <masadasif451@gmail.com>
2023-10-17 20:42:04 +0500

    Reverse an array in O(n)

---
+++ reverse_an_array.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size){
+    
+    int start = 0;
+    int end = size-1;
+    int temp = 0;
+    
+    while(start < end){
+        temp = arr[start];
+        
+        arr[start] = arr[end];
+        arr[end] = temp;
+        
+        start++;
+        end--;
+    }
+    
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    
+    reverseArray(arr, size);
+    
+    cout<<"The reverse array is : ";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
