5284406ae1f694ed2dc4e343eff832ff350a1c9b
Muhammad Asad <111152906+orientedasad@users.noreply.github.com>
2023-12-22 17:26:57 +0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,31 +1,34 @@
 //Sort the array by 0,1,2
+//Sort the array by 0,1,2
 #include<iostream>
 using namespace std;
 
-void find(int arr){
-    int size = sizeof(arr)/sizeof(arr[0]);
+void find(int arr[], int size){
     int l = 0;
     int m = 0;
-    ink r = size-1;
-
-    while(m<=r){
-      if(arr[m] == 0){
-        swap(arr[m], arr[l]);
-      }else if(arr[m] == 1){
-        m++;
-    }else if(arr[m] == 2){
-      swap(arr[m], arr[h])
+    int r = size-1;
+    
+    for(int i=0; i<size; i++){
+        if(arr[m] == 0){
+            swap(arr[m], arr[l]);
+            l++;
+            m++;
+        }else if(arr[m] == 1){
+            m++;
+        }else if(arr[m] == 2){
+            swap(arr[m], arr[r]);
+            r--;
+        }
     }
-    } 
-    return arr;
 }
 int main(){
-  int arr[] = {1,0,0,0,1,2,0,1,0,0,1};
-  int sarr[] = find(arr);
+    int arr[] = {2,0,0,0,1,2,0,1,0,0,1,1,2,0};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    find(arr, size);
 
-  for(int i=0; i<sarr.size(); i++){
-    cout<<sarr[i]<<" ";
-  }
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
 
   return 0;
 }
